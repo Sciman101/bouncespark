5a2903cafa00777e447fbc8a98ca761ee926f893
Sciman101 <Sciman1011@Gmail.com>
2024-06-09 13:20:49 -0400

    Stripped out pickups

--- source/main.lua
+++ source/main.lua
@@ -3,72 +3,16 @@ import "CoreLibs/timer"
 import "CoreLibs/crank"
 import "CoreLibs/sprites"
 
-import 'pool'
 import 'player'
-import 'pickup'
-import 'obstacle'
 
 -- Game constants
 local gfx <const> = playdate.graphics
 
 local player
-local pickups
-local obstacles
-
-local numberOfActivePickups = 0
-local score = 0
-
-slowdown = false -- nasty global eugh
-
-local function randomPos()
-    return math.random(16, 400 - 16), math.random(16, 240 - 16)
-end
-
-local function spawnPickup()
-    local pickup = pickups:get()
-    if pickup then
-        pickup:spawn(randomPos())
-        numberOfActivePickups += 1
-    end
-end
-
-local function onPickupCollected(pickup)
-    numberOfActivePickups -= 1
-    score += 1
-    pickups:reclaim(pickup)
-
-    if numberOfActivePickups == 1 then
-        local lastPickup = pickups.activeObjects[1]
-        if lastPickup then
-            lastPickup:disable()
-            pickups:reclaim(lastPickup)
-        else
-            local x, y = randomPos()
-            lastPickup = { x = x, y = y }
-        end
-        -- Spawn an obstacle
-        local obstacle = obstacles:get()
-        if obstacle then
-            obstacle:spawn(lastPickup.x, lastPickup.y)
-        end
-
-        -- Spawn 3 new pickups
-        for _ = 1, 3 do
-            spawnPickup()
-        end
-    elseif numberOfActivePickups == 0 then
-    end
-end
 
 local function setup()
     player = Player(200, 120)
     player:add()
-
-    pickups = Pool(Pickup, 10, onPickupCollected)
-    pickups:runOnInactive(function(p) p:add() end)
-
-    obstacles = Pool(Obstacle, 20)
-    obstacles:runOnInactive(function(p) p:add() end)
 
     gfx.sprite.setBackgroundDrawingCallback(
         function(x, y, width, height)
@@ -84,11 +28,6 @@ local function setup()
             player:stopAiming()
         end
     })
-
-    -- Spawn initial pickups
-    for _ = 1, 3 do
-        spawnPickup()
-    end
 end
 
 function playdate.update()
